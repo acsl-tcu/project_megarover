9df025b973dbe1c26b38a77366cc95022c55286f
Kazuma SEKIGUCHI <ksekiguc@tcu.ac.jp>
2025-01-20 01:43:35 +0900

    miroros

---
+++ 1_launcher/launch_arduino.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/bash
+
+source /root/.venv/bin/activate
+/root/arduino-ide_2.3.4_Linux_64bit.AppImage --no-sandbox
--- project_launch.sh
+++ project_launch.sh
@@ -1,8 +1,11 @@
 #! /usr/bin/bash +x
 
-source $ACSL_ROS2_DIR/bashrc
+source /home/sekiguchi/mega/.acsl/bashrc
 echo $PROJECT
 echo $ROS_DOMAIN_ID
 
-cd $ACSL_ROS2_DIR/0_host_commands/scripts
+cd /home/sekiguchi/mega/.acsl/0_host_commands/scripts
 dup microros
+### microros ###
+/home/sekiguchi/mega/.acsl/0_host_commands/scripts/dup microros
+### microros END ###
