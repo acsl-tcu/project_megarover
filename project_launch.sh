22aa9c800ad84d30f2fa806286227d148493972c
kasekiguchi <kasekiguchi@gmail.com>
2025-01-27 23:14:59 +0900

    megarover

--- project_launch.sh
+++ project_launch.sh
@@ -4,4 +4,5 @@ echo $PROJECT
 echo $ROS_DOMAIN_ID
 
 cd $ACSL_ROS2_DIR/0_host_commands/scripts/
-dup microros
+#dup microros
+docker run -it --rm -v /dev:/dev --privileged --net=host microros/micro-ros-agent:jazzy serial --dev /dev/ttyUSB-megarover -v4 --ros-args --remap __node:=microros_node --remap __ns:=/${HOSTNAME}
